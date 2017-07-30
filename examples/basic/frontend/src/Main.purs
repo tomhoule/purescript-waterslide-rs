14b4b1c58b23c84021870e368ea5d01844e49a01
Tom Houlé <tom@tomhoule.com>
2017-07-30 19:40:01 +0200

    Just use the Aeson codec for decoding

--- examples/basic/frontend/src/Main.purs
+++ examples/basic/frontend/src/Main.purs
@@ -1,23 +1,23 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, discard, pure, show, unit, ($))
 import Data.Generic (gShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Network.HTTP.Affjax (get, AJAX)
 import Data.Argonaut.Generic.Decode (genericDecodeJson)
+import Data.Argonaut.Generic.Aeson as Aeson
 import Data.Either (Either(..))
-import Control.Monad.Aff
-import Control.Monad.Eff.Exception
+import Control.Monad.Aff (Aff, liftEff', runAff)
+import Control.Monad.Eff.Exception (Error)
 
 import Data.Falafel (Meal)
-import Options as O
 
 getMeal :: forall eff. Aff (ajax :: AJAX, console :: CONSOLE | eff) (Either String Meal)
 getMeal = do
   res <- get "http://127.0.0.1:8077/meal"
   _ <- liftEff' $ log $ show res.response
-  pure $ genericDecodeJson O.options res.response
+  pure $ genericDecodeJson Aeson.options res.response
 
 logSuccess :: forall eff. Either String Meal -> Eff (console :: CONSOLE | eff) Unit
 logSuccess (Right meal) = log (gShow meal)
