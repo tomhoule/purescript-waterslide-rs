ef168040f5226e000307b284b2ed18ed3357a75e
Tom Houlé <tom@tomhoule.com>
2017-08-05 23:46:41 +0200

    Fix rustfmt failures

--- purescript_waterslide_derive/src/purescript.rs
+++ purescript_waterslide_derive/src/purescript.rs
@@ -20,7 +20,9 @@ impl<'a> ToTokens for VariantArguments<'a> {
             let tys = fields.iter().map(|f| &f.ty);
             tokens.append(quote!{
                 vec![
-                    #( <#tys as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor()  ),*
+                    #( <
+                       #tys as ::purescript_waterslide::ToPursConstructor
+                       >::to_purs_constructor()  ),*
                 ]
             })
         } else {
@@ -34,7 +36,9 @@ struct TupleField<'a>(&'a syn::Field);
 impl<'a> ToTokens for TupleField<'a> {
     fn to_tokens(&self, tokens: &mut Tokens) {
         let ty = &self.0.ty;
-        tokens.append(quote!{ <#ty as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor() })
+        tokens.append(quote!{
+            <#ty as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor()
+        })
     }
 }
 
@@ -49,7 +53,10 @@ impl<'a> ToTokens for RecordField<'a> {
             .unwrap_or("_unknown".to_string());
         let ty = &self.0.ty;
         tokens.append(quote!{
-            (#name.to_string(), <#ty as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor())
+            (
+                #name.to_string(),
+                <#ty as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor()
+            )
         })
     }
 }
@@ -63,7 +70,9 @@ pub fn make_purs_type(source: &DeriveInput) -> Result<Tokens, String> {
             let variant_arguments = variants.iter().map(VariantArguments);
             Ok(quote! {
                 ::purescript_waterslide::PursType::Enum(
-                    <#name#generics as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor(),
+                    <
+                    #name#generics as ::purescript_waterslide::ToPursConstructor
+                    >::to_purs_constructor(),
                     vec![
                         #( ::purescript_waterslide::PursConstructor {
                             name: #variant_names,
@@ -78,7 +87,9 @@ pub fn make_purs_type(source: &DeriveInput) -> Result<Tokens, String> {
             let purs_record_fields = fields.into_iter().map(RecordField);
             Ok(quote! {
                 ::purescript_waterslide::PursType::Struct(
-                    <#name#generics as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor(),
+                    <
+                    #name#generics as ::purescript_waterslide::ToPursConstructor
+                    >::to_purs_constructor(),
                     vec![
                         #( #purs_record_fields ),*
                     ],
@@ -89,7 +100,9 @@ pub fn make_purs_type(source: &DeriveInput) -> Result<Tokens, String> {
             let purs_tuple_fields = fields.iter().map(TupleField);
             Ok(quote! {
                 ::purescript_waterslide::PursType::TupleStruct(
-                    <#name#generics as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor(),
+                    <
+                    #name#generics as ::purescript_waterslide::ToPursConstructor
+                    >::to_purs_constructor(),
                     vec![
                         #( #purs_tuple_fields ),*
                     ],
@@ -117,7 +130,11 @@ pub fn make_purs_constructor_impl(ast: &DeriveInput) -> Result<Tokens, String> {
             name: #name.to_string(),
             module: None,
             parameters: vec![
-                #( <#parameters as ::purescript_waterslide::ToPursConstructor>::to_purs_constructor() ),*
+                #(
+                    <
+                    #parameters as ::purescript_waterslide::ToPursConstructor
+                    >::to_purs_constructor()
+                ),*
             ],
         }
     })
